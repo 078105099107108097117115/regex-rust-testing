71647a54449010e9782ff0d0ba13372108bfc555
Nicholas Njihia <njihianicklas55@gmail.com>
2020-06-06 18:57:22 +0300

    Added rust formatting with rustfmt and Set up lints with clippy and made warnings errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use regex_101::match_given_time_and_date;
 fn main() {
+    let y:usize = 10;
     let text = "Today's date is 2020-06-06 18:39pm";
     println!("{}", match_given_time_and_date(text));
     
