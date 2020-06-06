43f83628ac65c07b6c150a837a4753a3ec8aef5e
Nicholas Njihia <njihianicklas55@gmail.com>
2020-06-06 19:12:22 +0300

    Cargo fmt done

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 use regex::Regex;
 
 pub fn match_given_time_and_date(text: &str) -> bool {
     lazy_static! {
-        static ref RE : Regex = Regex::new(r"\d{4}-\d{2}-\d{2}[[:space:]]\d{2}:\d{2}[pa[m]]").unwrap();
+        static ref RE: Regex =
+            Regex::new(r"\d{4}-\d{2}-\d{2}[[:space:]]\d{2}:\d{2}[pa[m]]").unwrap();
     }
     RE.is_match(text)
 }
-
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,4 @@ use regex_101::match_given_time_and_date;
 fn main() {
     let text = "Today's date is 2020-06-06 18:39pm";
     println!("{}", match_given_time_and_date(text));
-    
 }
