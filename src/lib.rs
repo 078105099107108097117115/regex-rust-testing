e5f14aec3ce6afe9d6d2dd55b4401ec7f73a1b12
Nicholas Njihia <njihianicklas55@gmail.com>
2020-06-06 18:46:36 +0300

    Regex parsing time and date

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
 #[macro_use] extern crate lazy_static;
 use regex::Regex;
 
-pub fn match_text(text: &str) -> bool {
+pub fn match_given_time_and_date(text: &str) -> bool {
     lazy_static! {
-        static ref RE : Regex = Regex::new(r"\d{4}-\d{2}-\d{2}").unwrap();
+        static ref RE : Regex = Regex::new(r"\d{4}-\d{2}-\d{2}[[:space:]]\d{2}:\d{2}[pa[m]]").unwrap();
     }
     RE.is_match(text)
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn match_text_test() {
-        let text = "This is the date today : 2020-06-06";
-        assert!(match_text(text));
+        let text = "This is the date and time today : 2020-06-06 12:30pm";
+        assert!(match_given_time_and_date(text));
     }
 }
